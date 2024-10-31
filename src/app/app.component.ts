364b5061963bc1ba14b43831c90968cde7dad2c4
Ferenc Deak <ferenc.deak@accesa.eu>
2024-10-31 21:17:08 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BeneficiariComponent } from './beneficiari/beneficiari.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, BeneficiariComponent, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'own-app';
---
+++ src/app/beneficiari/beneficiari.component.ts
@@ -0,0 +1,162 @@
+import { Component, OnInit } from '@angular/core';
+import { Beneficiar } from './beneficiar.model';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-beneficiari',
+  standalone: true,
+  templateUrl: './beneficiari.component.html',
+  styleUrls: ['./beneficiari.component.scss'],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
+})
+export class BeneficiariComponent implements OnInit {
+  beneficiari: Beneficiar[] = [];
+  persoane: any[] | undefined;
+  newBeneficiar: Beneficiar = this.initializeNewBeneficiar();
+  isEditing: boolean = false;
+  isSubmitted = false;
+  currentBeneficiarId: number | null = null;
+  filteredBeneficiari: Beneficiar[] = [];
+  sortColumn: string | null = null;
+  sortDirection: 'asc' | 'desc' = 'asc';
+
+  ngOnInit(): void {
+    this.loadBeneficiari();
+    this.persoane = [
+      { id: 1, name: 'Persoană Fizică', value: 'persoana_fizica' },
+      { id: 2, name: 'Persoană Juridică', value: 'persoana_juridica' },
+    ];
+    this.filteredBeneficiari = [...this.beneficiari];
+  }
+
+  initializeNewBeneficiar(): Beneficiar {
+    return {
+      tip: 'persoana_fizica',
+      nume: '',
+      prenume: '',
+      adresa: '',
+      cnp: '',
+      dataInfiintarii: '',
+      dataNasterii: '',
+      telefon: '',
+      conturiIBAN: '',
+    };
+  }
+
+  filterBeneficiari(event: KeyboardEvent) {
+    const target = event.target as HTMLInputElement;
+    const filterValue = target.value.toLowerCase();
+
+    this.filteredBeneficiari = this.beneficiari.filter((beneficiar) => {
+      const numeMatch =
+        beneficiar.nume?.toLowerCase().includes(filterValue) || false;
+      const prenumeMatch =
+        beneficiar.prenume?.toLowerCase().includes(filterValue) || false;
+      const denumire =
+        beneficiar.denumire?.toLowerCase().includes(filterValue) || false;
+      const tipMatch =
+        beneficiar.tip?.toLowerCase().includes(filterValue) || false;
+      return numeMatch || prenumeMatch || denumire || tipMatch;
+    });
+  }
+
+  loadBeneficiari() {
+    if (typeof window !== 'undefined') {
+      const data = localStorage.getItem('beneficiari');
+      this.beneficiari = data ? JSON.parse(data) : [];
+      this.filteredBeneficiari = [...this.beneficiari];
+    }
+  }
+
+  saveBeneficiar() {
+    this.isSubmitted = true;
+    if (this.validateBeneficiar(this.newBeneficiar)) {
+      if (this.isEditing && this.currentBeneficiarId !== null) {
+        this.updateBeneficiarInList();
+      } else {
+        this.newBeneficiar.id = Date.now();
+        this.beneficiari.push({ ...this.newBeneficiar });
+      }
+      localStorage.setItem('beneficiari', JSON.stringify(this.beneficiari));
+      this.updateFilteredList();
+      this.resetForm();
+    }
+  }
+
+  updateBeneficiar(beneficiar: Beneficiar) {
+    this.newBeneficiar = { ...beneficiar };
+    this.isEditing = true;
+    this.currentBeneficiarId = beneficiar.id!;
+  }
+
+  updateBeneficiarInList() {
+    const index = this.beneficiari.findIndex(
+      (b) => b.id === this.currentBeneficiarId
+    );
+    if (index !== -1) {
+      this.beneficiari[index] = { ...this.newBeneficiar };
+    }
+    this.updateFilteredList();
+    this.isEditing = false;
+    this.currentBeneficiarId = null;
+  }
+
+  deleteBeneficiar(id: number) {
+    this.beneficiari = this.beneficiari.filter((b) => b.id !== id);
+    localStorage.setItem('beneficiari', JSON.stringify(this.beneficiari));
+    this.updateFilteredList();
+  }
+
+  validateBeneficiar(beneficiar: Beneficiar): boolean {
+    const mandatoryFields: (keyof Beneficiar)[] =
+      beneficiar.tip === 'persoana_juridica'
+        ? ['cui', 'conturiIBAN']
+        : ['cnp', 'conturiIBAN'];
+
+    return (
+      mandatoryFields.every((field) => {
+        const value = beneficiar[field];
+        return (
+          typeof value === 'string' && value.length > 0 && value.length <= 100
+        );
+      }) &&
+      beneficiar.adresa.length <= 100 &&
+      beneficiar.telefon.length <= 100 &&
+      beneficiar.conturiIBAN.length <= 100
+    );
+  }
+
+  resetForm() {
+    this.newBeneficiar = this.initializeNewBeneficiar();
+    this.isEditing = false;
+    this.currentBeneficiarId = null;
+    this.updateFilteredList();
+  }
+
+  updateFilteredList() {
+    this.filteredBeneficiari = [...this.beneficiari];
+  }
+
+  sortTable(column: keyof Beneficiar) {
+    if (this.sortColumn === column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortDirection = 'asc';
+    }
+    this.sortColumn = column;
+
+    this.filteredBeneficiari.sort((a, b) => {
+      const valueA = a[column] || '';
+      const valueB = b[column] || '';
+
+      const comparison = valueA
+        .toString()
+        .localeCompare(valueB.toString(), undefined, {
+          numeric: true,
+        });
+
+      return this.sortDirection === 'asc' ? comparison : -comparison;
+    });
+  }
+}
